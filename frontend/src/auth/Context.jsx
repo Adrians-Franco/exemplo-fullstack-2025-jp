05ed3f3ec2dea0820e449838ed0504154b295f99
Renan <warreir@gmail.com>
2025-11-19 20:56:31 -0300

    add auth

--- frontend/src/auth/Context.jsx
+++ frontend/src/auth/Context.jsx
@@ -1,10 +1,22 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext()
 // token do usuario
 export const AuthProvider = ({ children }) => {
+    const [token, setToken] = useState(null)
+
+    const login = (token) => {
+        setToken(token)
+        localStorage.setItem('token', token)
+    }
+
+    const logout = () => {
+        setToken(null)
+        localStorage.removeItem('token')
+    }
+
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={{ token, login, logout }}>
             { children }
         </AuthContext.Provider>
     )
